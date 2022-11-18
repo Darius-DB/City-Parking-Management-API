77311489f0484f60523c6020b8549e4cf962c9e3
darius <dariusboengiu@gmail.com>
2022-11-18 09:35:51 +0200

    Created project with Spring Initializr

---
+++ src/main/java/com/goosfraba/city_parking/CityParkingApplication.java
@@ -0,0 +1,13 @@
+package com.goosfraba.city_parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goosfraba/city_parking/CityParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goosfraba.city_parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
